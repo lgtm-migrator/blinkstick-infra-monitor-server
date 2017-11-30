cfb10b1d19f0218dbc8aeb2a1af7abb8a8c77384
Sean Dawson <spdawson@agiledigital.com.au>
2017-11-30 16:34:19 +1100

    Fix caching of JSON data

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const fs = require('fs');
 
 app.get('/health_data', (req, res) => {
     console.log("Pretending to gather health data...");
-    const healthData = require('./healthdata.json');
+    const healthData = JSON.parse(fs.readFileSync('./healthdata.json', 'utf8'));
     res.json(healthData);
 })
 
-app.listen(3000, () => console.log('Listening on port 3000!'))
+app.listen(3000, () => console.log('Listening on port 3000!'));
